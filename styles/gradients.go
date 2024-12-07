345ad2e2f6d55dd21cc5fe27e890a8a2e869c768
juanatsap <juanatsap@gmail.com>
2024-12-07 19:33:09 +0000

    added gradients

--- styles/gradients.go
+++ styles/gradients.go
@@ -13,6 +13,14 @@ var (
 	maxVal     = 1000.0
 	startColor = color.NRGBA{R: 0xF8, G: 0xD4, B: 0x95, A: 0xFF} // #F8D495
 	endColor   = color.NRGBA{R: 0xFF, G: 0x00, B: 0x00, A: 0xFF} // #FF0000
+
+	// Rango para ingresos positivos
+	minIncomeVal     = 0.0
+	maxIncomeVal     = 3000.0
+	startGreenColor  = color.NRGBA{R: 0xD4, G: 0xF8, B: 0xD4, A: 0xFF} // #D4F8D4 (Verde pastel)
+	endGreenColor    = color.NRGBA{R: 0x00, G: 0xFF, B: 0x00, A: 0xFF} // #00FF00 (Verde intenso)
+	negativeRedColor = color.NRGBA{R: 0xFF, G: 0x00, B: 0x00, A: 0xFF} // #FF0000 (Rojo para negativos)
+
 )
 
 func GetStyleForAmount(amount float64) *widget.CustomTextGridStyle {
@@ -61,4 +69,52 @@ func GetStyleForAmount(amount float64) *widget.CustomTextGridStyle {
 	}
 
 	return &widget.CustomTextGridStyle{FGColor: &frontendColor, BGColor: &c}
+}
+
+func GetStyleForBalance(balance float64) *widget.CustomTextGridStyle {
+	var bgColor color.NRGBA
+
+	if balance < 0 {
+		// Caso negativo: fondo rojo fijo
+		bgColor = negativeRedColor
+	} else {
+		// Caso positivo o cero: interpolar entre startGreenColor y endGreenColor
+		val := balance
+		if val > maxIncomeVal {
+			val = maxIncomeVal // Limitar por arriba
+		}
+
+		fraction := (val - minIncomeVal) / (maxIncomeVal - minIncomeVal)
+		if fraction < 0 {
+			fraction = 0
+		} else if fraction > 1 {
+			fraction = 1
+		}
+
+		R := float64(startGreenColor.R) + fraction*(float64(endGreenColor.R)-float64(startGreenColor.R))
+		G := float64(startGreenColor.G) + fraction*(float64(endGreenColor.G)-float64(startGreenColor.G))
+		B := float64(startGreenColor.B) + fraction*(float64(endGreenColor.B)-float64(startGreenColor.B))
+		A := float64(startGreenColor.A) + fraction*(float64(endGreenColor.A)-float64(startGreenColor.A))
+
+		bgColor = color.NRGBA{
+			R: uint8(math.Round(R)),
+			G: uint8(math.Round(G)),
+			B: uint8(math.Round(B)),
+			A: uint8(math.Round(A)),
+		}
+	}
+
+	// Calcular luminancia para determinar el color de texto
+	luminance := (0.299*float64(bgColor.R) + 0.587*float64(bgColor.G) + 0.114*float64(bgColor.B)) / 255.0
+
+	var fgColor color.NRGBA
+	if luminance > 0.5 {
+		// Fondo claro, texto oscuro
+		fgColor = color.NRGBA{R: 0x00, G: 0x00, B: 0x00, A: 0xFF}
+	} else {
+		// Fondo oscuro, texto claro
+		fgColor = color.NRGBA{R: 0xFF, G: 0xFF, B: 0xFF, A: 0xFF}
+	}
+
+	return &widget.CustomTextGridStyle{FGColor: &fgColor, BGColor: &bgColor}
 }
